d94405caa8e2b9772680a99bc025621923b58f8f
koukyo1994 <arabiannight1994@yahoo.co.jp>
2018-06-07 22:42:08 +0900

    working on getting marker from web cam but found it doesn't work

--- src/detect_ar_marker/main.cpp
+++ src/detect_ar_marker/main.cpp
@@ -2,16 +2,12 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/aruco.hpp>
-#include <vector>
-#include <opencv2/videoio.hpp>
-#include <opencv2/aruco/charuco.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 
 int main(int argc, char **argv) {
   cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
   cv::VideoCapture inputVideo;
-  cv::Mat cameraMatrix, distCoeffs;
-  cv::aruco::readCameraParameters(cameraMatrix, distCoeffs);
 
   inputVideo.open(0);
   while (inputVideo.grab()) {
@@ -22,6 +18,17 @@ int main(int argc, char **argv) {
     std::vector<int> ids;
     std::vector<std::vector<cv::Point2f> > corners;
 
+    cv::aruco::detectMarkers(image, dictionary, corners, ids);
+    if (ids.size() > 0){
+      cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
+      for (std::vector<cv::Point2f>::const_iterator i = corners[0].begin(); i != corners[0].end(); i++) {
+        std::cout << *i << std::endl;
+      }
+    }
+
+    cv::imshow("out", imageCopy);
+    char key = (char) cv::waitKey(1);
+    if (key == 27)
+      break;
   }
-  return 0;
 }
